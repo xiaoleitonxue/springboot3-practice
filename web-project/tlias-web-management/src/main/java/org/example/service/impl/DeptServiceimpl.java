c52f25a8c0dfe4170887db3f6482c149f76eb4c0
陈宇 <3128607571@qq.com>
2026-03-27 17:40:49 +0800

    tlias-web-practice

---
+++ web-project/tlias-web-management/src/main/java/org/example/service/impl/DeptServiceimpl.java
@@ -0,0 +1,8 @@
+package org.example.service.impl;
+
+import org.example.service.DeptService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class DeptServiceimpl implements DeptService {
+}
