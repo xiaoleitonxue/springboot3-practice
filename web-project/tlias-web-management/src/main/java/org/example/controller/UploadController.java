0f6e3302bc1ea311a1f439fa119edf4f106ca6b1
陈宇 <3128607571@qq.com>
2026-04-01 11:54:11 +0800

    tlias-web-project

--- web-project/tlias-web-management/src/main/java/org/example/controller/UploadController.java
+++ web-project/tlias-web-management/src/main/java/org/example/controller/UploadController.java
@@ -2,6 +2,8 @@ package org.example.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.example.pojo.Result;
+import org.example.utils.AliyunOSSOperator;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,7 +18,7 @@ import java.util.UUID;
 @RestController
 public class UploadController {
 
-    @PostMapping("/upload")
+    /*@PostMapping("/upload")
     public Result upload(String name,Integer age,  MultipartFile file) throws IOException {
         log.info("上传文件，参数：{},{},{}", name, age, file);
 
@@ -24,6 +26,18 @@ public class UploadController {
 
         file.transferTo(new File("D:/images/" + newFileName));
         return Result.success();
+    }*/
+    @Autowired
+    private AliyunOSSOperator aliyunOSSOperator;
+
+    @PostMapping("/upload")
+    public Result upload(MultipartFile file) throws Exception {
+        log.info("上传文件，参数：{}", file.getOriginalFilename());
+
+        String url = aliyunOSSOperator.upload(file.getBytes(), file.getOriginalFilename());
+        log.info("上传结果：{}", url);
+
+        return Result.success(url);
     }
 
 
