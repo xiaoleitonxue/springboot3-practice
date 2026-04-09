c385f8d5de72f5328438d6ec74f5ac66a5865aee
陈宇 <3128607571@qq.com>
2026-04-09 11:35:47 +0800

    tlias-web-project

---
+++ web-project/springboot-aop-quickstart/src/main/java/com/itheima/aop/RecorrdTimeAspect.java
@@ -0,0 +1,25 @@
+package com.itheima.aop;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@Aspect
+public class RecorrdTimeAspect {
+
+    @Around("execution(* com.itheima.service.impl.*.*(..))")
+    public Object recordTime(ProceedingJoinPoint pjp) throws Throwable {
+        long start = System.currentTimeMillis();
+
+        Object result = pjp.proceed();
+
+        long end = System.currentTimeMillis();
+        log.info("方法{}执行耗时：{}", pjp.getSignature(), end - start);
+
+        return result;
+    }
+}
